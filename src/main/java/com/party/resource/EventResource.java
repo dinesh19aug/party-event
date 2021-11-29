ca3277653b36ab15226c8ca9f17ea218b82528fa
BEAST-PC\dines <dinesh19aug@gmail.com>
2021-11-28 20:56:23 -0500

    Added neo4j and sample movie resource to get list of movies

--- src/main/java/com/party/resource/EventResource.java
+++ src/main/java/com/party/resource/EventResource.java
@@ -1,31 +1,52 @@
 
 package com.party.resource;
 
-import com.party.service.IEventService;
 import com.party.service.impl.EventService;
 import com.party.vo.Event;
 import javax.inject.Inject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+
 import javax.ws.rs.core.Response;
+
+import com.party.vo.Movie;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.async.AsyncSession;
+
+import java.util.concurrent.CompletionStage;
 
 /**
  *
  * @author dines
  */
-@Path("/party/user")
+@Path("/party")
 
 public class EventResource {
     @Inject
     EventService eventService;
-    
+    @Inject
+    Driver driver;
     @GET
+    @Path("event")
     //@Produces(MediaType.APPLICATION_JSON)
     public Response getEvent(){
         Event event = eventService.execute();
         return Response.ok(event).build();
+    }
+    @GET
+    @Path("movie")
+    public CompletionStage<Response> getMovie(){
+        AsyncSession session = driver.asyncSession();
+        return session
+                .runAsync("MATCH (m:Movie) RETURN m ORDER BY m.title")
+                .thenCompose(cursor ->
+                        cursor.listAsync(record -> Movie.from(record.get("m").asNode()))
+                )
+                .thenCompose(movies ->
+                        session.closeAsync().thenApply(signal -> movies)
+                )
+                .thenApply(Response::ok)
+                .thenApply(Response.ResponseBuilder::build);
     }
 }
--- src/test/java/org/acme/EventResourceTest.java
+++ src/test/java/org/acme/EventResourceTest.java
@@ -7,15 +7,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+public class EventResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/party/event")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .statusCode(200);
+             //.body(is("Hello RESTEasy"));
     }
 
 }
